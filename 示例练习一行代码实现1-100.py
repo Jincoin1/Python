b320cc1506a63f73d86032c40ac34a0a87849470
JinCoin <149879413+Jincoin1@users.noreply.github.com>
2025-02-16 20:47:21 +0800

    Add files via upload

---
+++ 示例练习一行代码实现1-100.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+if __name__ == "__main__":
+    total = 0
+    total2 = 0
+    for i in range(101):
+        total += i
+    print('total:',total)
+
+    for a in range(101):
+            total2 += a
+    print('total2:', total2)
+            
+    for c in range (3):
+            print(c)
